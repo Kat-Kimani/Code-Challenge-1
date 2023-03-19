5ccf758bbe62787cba5d31f91658945bf2cf9311
catherine <wachukimani@gmail.com>
2023-03-19 13:33:55 +0300

    working on this

--- README.md
+++ README.md
@@ -16,3 +16,23 @@ assign your output to the finalOutput variable
 
 # YOUR README DESCRIPTION STARTS HERE
 
+Student Grade Generator
+This is a simple web application that generates a grade based on marks scored. 
+
+How to Use our simple calculator. Simply enter the marks in the input field and click the "Get Grade" button. The application will show you what grade was achieved by the respective marks
+
+A: 80-100
+B: 60-79
+C: 50-59
+D: 40-49
+E: < 40
+
+If the input is not a number or falls outside of these ranges, the application will display an error message. Invalid Output
+
+Technologies Used
+This application was built using HTML, CSS, and JavaScript.
+
+
+
+Authors
+This application was developed by [kat Kimani]. If you have any questions or feedback, please feel free to contact me through my github kat-kimani.
--- script.js
+++ script.js
@@ -1,57 +1,87 @@
-let grade=document.getElementById('grade')
-let speed=document.getElementById('speed')
-let salary=document.getElementById('salary')
+let grade = document.getElementById('grade')
+let speed = document.getElementById('speed')
+let salary = document.getElementById('salary')
 
 
-grade.addEventListener('click', (e)=>{
-e.preventDefault()
-let inputGrade=parseInt(document.getElementById('input-grade').value)
-console.log(inputGrade)
-let finalOutput
-// write Code For the Code challenge
+grade.addEventListener('click', (e) => {
+    e.preventDefault()
+    let marks = parseInt(document.getElementById('input-grade').value)
+    console.log(marks)
+
+    let finalOutput = ''
+    // write Code For the Code challenge
+
+    
+    if (marks > 79 && marks<=100) {
+        finalOutput ='A'
+    } else if (marks => 60 && marks < 79) {
+
+        finalOutput ='B'
+    } else if (marks => 49 && marks <= 59) {
+
+        finalOutput ='C'
+
+    } else if (marks => 40 && marks < 49) {
+        finalOutput='D'
+
+    } else if (marks < 40) {
+        finalOutput='E'
+    } else {
+        finalOutput= 'Invalid input. Please Enter correct marks'
+
+    }
 
 
-
-
-
-
-
-
-
-
-finalOutput='assign your final output to this variable'
-document.getElementById('marks-id').append(finalOutput)
+   //finalOutput = ''
+    document.getElementById('marks-id').append(finalOutput)
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 speed.addEventListener('click', (e)=>{
     e.preventDefault()
-    let inputSpeed=parseInt(document.getElementById('input-speed').value)
-    console.log(inputSpeed)
-    let finalOutput
+    let inputSpeed=parseInt(document.getElementById('speed').value)
+    console.log(input-speed)
+    let finalOutput=''
 // write Code For the Code challenge
 
+function speedCheck(speed) {
+    const demeritPoints = Math.floor((speed - 70) / 5);
+    if (inputSpeed <= 70) {
+        finalOutput = "Ok"
+    } else if (demeritPoints >= 1 && demeritPoints <= 12) {
+        finalOutput = "Demerit points: " + demeritPoints
+    } else if (demeritPoints > 12) {
+        finalOutput = "License suspended"
+    }
+}
+speedCheck(speed)
 
 
 
 
 
 
-
-
-
-
-
-
-finalOutput='assign your final output to this variable'
+//finalOutput='assign your final output to this variable'
     document.getElementById('speed-id').append(finalOutput)
 
 })
-salary.addEventListener('click', (e)=>{
-        e.preventDefault()
-        let inputSalary=parseInt(document.getElementById('input-salary').value)
-        console.log(inputSalary)
-let finalOutput
-// write Code For the Code challenge
 
 
 
@@ -63,7 +93,27 @@ let finalOutput
 
 
 
-finalOutput ='assign your final output to this variable'
-document.getElementById('salary-id').append(finalOutput)
-        
+
+
+
+salary.addEventListener('click', (e) => {
+    e.preventDefault()
+    let inputSalary = parseInt(document.getElementById('input-salary').value)
+    console.log(inputSalary)
+    let finalOutput
+    // write Code For the Code challenge
+
+
+
+
+
+
+
+
+
+
+
+    finalOutput = 'assign your final output to this variable'
+    document.getElementById('salary-id').append(finalOutput)
+
 })
