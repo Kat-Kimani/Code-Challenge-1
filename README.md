dc181ba13ce9e7857dd7d95fb9f6da1bf81421ca
catherine <wachukimani@gmail.com>
2023-03-19 18:38:41 +0300

    completed code challenge

--- README.md
+++ README.md
@@ -1,22 +1,10 @@
 # Code_challenge
 
 
-inputGrade-variable for the input Garde
-
-inputSpeed-variable for the input Speed
-
-inputSalary-Variable for the Input Salary
-
-
-your Code should come after
-// write Code For the Code challenge
-
-assign your output to the finalOutput variable
-
-
 # YOUR README DESCRIPTION STARTS HERE
 
-Student Grade Generator
+STUDENT GRADE GENERATOR
+DESCRIPTION
 This is a simple web application that generates a grade based on marks scored. 
 
 How to Use our simple calculator. Simply enter the marks in the input field and click the "Get Grade" button. The application will show you what grade was achieved by the respective marks
@@ -32,7 +20,99 @@ If the input is not a number or falls outside of these ranges, the application w
 Technologies Used
 This application was built using HTML, CSS, and JavaScript.
 
+PROJECT SET UP
+To set up this project, you will need to have the following installed:
+
+A web browser such as Google Chrome, Mozilla Firefox, or Microsoft Edge
+A text editor such as Visual Studio Code, Atom, or Sublime Text
+
+To run the project, follow these steps:
+
+1. Fork the lesson from github and clone to your local machine
+2. OR create a new file on your text editor
+3. Save the file with a .html extension (e.g. "index.html").
+4. Open the file in your web browserusing explorer.exe index.html
+5. You should now see a simple webpage with an input field and a button. 
+6. You can enter MARKS in the input field and click the button to see the GRADE ACQUIRED  or an error message if invalid output is given message asking you to provide valid input. marks can only range from 1 to 100.
+
+
+------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+
+
+
+THE SPEED CALCULATOR
+DESCRIPTION
+The code provided is a basic implementation of a speed detector using JavaScript & HTML
+
+The HTML provides an input field for the user to enter the speed and a submit button that triggers the JavaScript code to calculate the number of demerit points, if the speed given is above the allowed 70KPH. It also goes ahead to advice whether the license should be suspended if the demerit points exceed 12points.
+
+The JavaScript code includes an event listener that listens for a "click" event on the submit button. When the button is clicked, the code gets the input speed value entered by the user, converts it to an integer using parseInt(), and passes it to the speedCheck() function to calculate the number of demerit points or whether the license should be suspended.
+
+The speedCheck() function uses a formula to calculate the demerit points and some conditional statements to determine the output.
+
+Finally, the output is displayed on the web page using the document.getElementById() method to access the HTML element with the id "speed-id" and the append() method to add any text given in the final output to the element.
+
+The code can be improved by adding error handling, better user interface design, and more features to enhance the functionality of the speed detector.
+
+PROJECT SET UP
+To set up this project, you will need to have the following installed:
+
+A web browser such as Google Chrome, Mozilla Firefox, or Microsoft Edge
+A text editor such as Visual Studio Code, Atom, or Sublime Text
+
+To run the project, follow these steps:
+
+1. Fork the lesson from github and clone to your local machine
+2. OR create a new file on your text editor
+3. Save the file with a .html extension (e.g. "index.html").
+4. Open the file in your web browserusing explorer.exe index.html
+5. You should now see a simple webpage with an input field and a button. 
+6. You can enter a speed value in the input field and click the button to see the demerit points or "Ok" message displayed on the page.
+7. License suspended will display if the speed is too high calculating more that 12 demerit points
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+NET SALARY CALCULATOR
+DESCRIPTION
+
+This is a salary calculator web application that calculates the net salary of an employee based on their gross salary, tax deductions, and other deductions such as NSSF and NHIF. It is built using HTML, CSS, and JavaScript.
+
+Features
+Input field for user to enter gross salary
+Calculates tax deductions (PAYE) based on taxable income
+Calculates NSSF and NHIF deductions based on gross salary
+Displays the net salary after all deductions have ben made to the user
+can be useful to help you calculate desired salary as a net of all statutory deductions.
+
+
+PROJECT SET UP
+To set up this project, you will need to have the following installed:
+
+A web browser such as Google Chrome, Mozilla Firefox, or Microsoft Edge
+A text editor such as Visual Studio Code, Atom, or Sublime Text
+
+To run the project, follow these steps:
+
+1. Fork the lesson from github and clone to your local machine
+2. OR create a new file on your text editor
+3. Copy and paste the code you provided into the new file.
+4. Save the file with a .html extension (e.g. "index.html").
+5. Open the file in your web browserusing explorer.exe index.html
+6. You should now see a simple webpage with an input field and a button. 
+7. You can enter a speed value in the input field and click the button to see the demerit points or "Ok" message displayed on the page.
+8. License suspended will display if the speed is too high calculating more that 12 demerit points
+
+
+
+
+
+
 
 
 Authors
 This application was developed by [kat Kimani]. If you have any questions or feedback, please feel free to contact me through my github kat-kimani.
+
+License
+This project is licensed under the GIT License. See the LICENSE file for details.
--- script.js
+++ script.js
@@ -2,37 +2,37 @@ let grade = document.getElementById('grade')
 let speed = document.getElementById('speed')
 let salary = document.getElementById('salary')
 
+// Add an event listener to the 'grade' button
+document.getElementById('grade').addEventListener('click', (e) => {
+    e.preventDefault() // Prevent page from refreshing on submit
+    let inputGrade = parseInt(document.getElementById('input-grade').value)
+    console.log(inputGrade) // Log the input grade to the console
 
-grade.addEventListener('click', (e) => {
-    e.preventDefault()
-    let marks = parseInt(document.getElementById('input-grade').value)
-    console.log(marks)
-
-    let finalOutput = ''
+    let finalOutput // Declare a variable for the final output
     // write Code For the Code challenge
 
-    
-    if (marks > 79 && marks<=100) {
-        finalOutput ='A'
-    } else if (marks => 60 && marks < 79) {
+    // Check the marks scored and assign the corresponding letter grade
+    if (inputGrade > 79 && inputGrade <= 100) {
+        finalOutput = 'A'
 
-        finalOutput ='B'
-    } else if (marks => 49 && marks <= 59) {
+    } else if (inputGrade >= 60 && inputGrade <= 79) {
+        finalOutput = 'B'
 
-        finalOutput ='C'
+    } else if (inputGrade >= 49 && inputGrade <= 59) {
+        finalOutput = 'C'
 
-    } else if (marks => 40 && marks < 49) {
-        finalOutput='D'
+    } else if (inputGrade >= 40 && inputGrade < 49) {
+        finalOutput = 'D'
 
-    } else if (marks < 40) {
-        finalOutput='E'
+    } else if (inputGrade < 40) {
+        finalOutput = 'E'
     } else {
-        finalOutput= 'Invalid input. Please Enter correct marks'
+        finalOutput = 'Invalid input. Please Enter correct marks'
 
     }
 
 
-   //finalOutput = ''
+
     document.getElementById('marks-id').append(finalOutput)
 
 })
@@ -53,32 +53,44 @@ grade.addEventListener('click', (e) => {
 
 
 
+//an event listener that listens for a "click" event on the submit button
 
-speed.addEventListener('click', (e)=>{
-    e.preventDefault()
-    let inputSpeed=parseInt(document.getElementById('speed').value)
-    console.log(input-speed)
-    let finalOutput=''
-// write Code For the Code challenge
+document.getElementById('submit-speed').addEventListener('click', (e) => {
+    e.preventDefault()// Prevent page from refreshing on submit
+    // which would cause the page to reload when the button is clicked.
 
-function speedCheck(speed) {
-    const demeritPoints = Math.floor((speed - 70) / 5);
-    if (inputSpeed <= 70) {
-        finalOutput = "Ok"
-    } else if (demeritPoints >= 1 && demeritPoints <= 12) {
-        finalOutput = "Demerit points: " + demeritPoints
-    } else if (demeritPoints > 12) {
-        finalOutput = "License suspended"
+    //parseInt() converts the value input to an integer
+    let inputSpeed = parseInt(document.getElementById('input-speed').value)
+    console.log(inputSpeed)     // Log the input speed to the console
+    let finalOutput             // Log the input grade to the console
+
+
+    // write Code For the Code challenge
+
+
+    //Check the speed and calculate the demerit points
+    function speedCheck(inputSpeed) {
+        const demeritPoints = Math.floor((inputSpeed - 70) / 5);
+        if (inputSpeed <= 70) {
+            finalOutput = "Ok"
+        } else if (demeritPoints >= 1 && demeritPoints <= 12) {
+            finalOutput = "Demerit points: " + demeritPoints
+        } else if (demeritPoints > 12) {
+            finalOutput = "License suspended"
+        }
+        return finalOutput
     }
-}
-speedCheck(speed)
 
 
 
 
 
 
-//finalOutput='assign your final output to this variable'
+    //calls the speedCheck function which has the speed parameter
+    finalOutput = speedCheck(inputSpeed);
+
+    //access  HTML element with the id "speed-id".
+    //.append(finalOutput) is a method that adds the text in the finalOutput 
     document.getElementById('speed-id').append(finalOutput)
 
 })
@@ -96,24 +108,83 @@ speedCheck(speed)
 
 
 
-salary.addEventListener('click', (e) => {
+document.getElementById('salary').addEventListener('click', (e) => {
     e.preventDefault()
-    let inputSalary = parseInt(document.getElementById('input-salary').value)
-    console.log(inputSalary)
+    let grossSalary = parseInt(document.getElementById('input-salary').value)
+    console.log(grossSalary)
     let finalOutput
     // write Code For the Code challenge
 
+    function calculateNetSalary(grossSalary) {
+        let taxableIncome = grossSalary - 2400; // remove personal relief of 2400 per month
+        let paye = 0;           // initialize PAYE to 0
+        let nssf = 400;         // initialize NSSF to 400 per month
+        let nhif = 0;           // initialize NHIF to 0
+
+        // compute PAYE based on taxable income
+
+        if (taxableIncome <= 24000) {
+            paye = taxableIncome * 0.1
+
+        } else if (taxableIncome >= 24001 && taxableIncome <= 32333) {
+            paye = taxableIncome * 0.25
+
+        } else if (taxableIncome > 32333) {
+            paye = taxableIncome * 0.3
+
+        }
+
+
+        // compute NHIF based on gross salary
+        if (grossSalary <= 5999) {
+            nhif = 150
+        } else if (grossSalary <= 7999) {
+            nhif = 300
+        } else if (grossSalary <= 11999) {
+            nhif = 400
+        } else if (grossSalary <= 14999) {
+            nhif = 500
+        } else if (grossSalary <= 19999) {
+            nhif = 600
+        } else if (grossSalary <= 24999) {
+            nhif = 750
+        } else if (grossSalary <= 29999) {
+            nhif = 850
+        } else if (grossSalary <= 34999) {
+            nhif = 900
+        } else if (grossSalary <= 39999) {
+            nhif = 950
+        } else if (grossSalary <= 44999) {
+            nhif = 1000
+        } else if (grossSalary <= 49999) {
+            nhif = 1100
+        } else if (grossSalary <= 59999) {
+            nhif = 1200
+        } else if (grossSalary <= 69999) {
+            nhif = 1300
+        } else if (grossSalary <= 79999) {
+            nhif = 1400
+        } else if (grossSalary <= 89999) {
+            nhif = 1500
+        } else if (grossSalary <= 99999) {
+            nhif = 1600
+        } else if (grossSalary > 100000) {
+            nhif = 1700
+        }
+
+
+        // calculate the net salary
+        let netSalary = grossSalary - paye - nssf - nhif;
+        return netSalary
+
+    }
 
 
 
 
 
 
-
-
-
-
-    finalOutput = 'assign your final output to this variable'
+    finalOutput = calculateNetSalary(grossSalary)
     document.getElementById('salary-id').append(finalOutput)
 
 })
